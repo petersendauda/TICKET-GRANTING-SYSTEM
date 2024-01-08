4f901df5f0669117dd94f1f3c4798f7f79bd9b7d
Emmanuel Sahr Dauda <123657141+petersendauda@users.noreply.github.com>
2024-01-08 17:02:26 +0000

    Delete ticket/include directory

--- ticket/include/dbconn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "tickets";
-    $conn = mysqli_connect($servername, $username, $password, $dbname)or die('could not connect to database');
-// echo "Hello!!!";
-?>
